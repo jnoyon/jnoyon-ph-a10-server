07efd6d6e638c40f4a7e5317e6319292eebd8250
jnoyon <jihadur51@gmail.com>
2025-01-02 17:26:32 +0600

    Read Write and Update Added

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const cors = require('cors')
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
 app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.puu64.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,17 +21,42 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const visaCollection = client.db('visaDB').collection('visa')
+
+
+    app.get('/visa', async(req, res) => {
+      const cursor = visaCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/visa', async(req, res) => {
+      const newVisa = req.body;
+      console.log(newVisa)
+      const result = await visaCollection.insertOne(newVisa);
+      res.send(result);
+    })
+    
+    app.delete('/visa/:id', async(req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const query = {_id: new ObjectId(id)}
+      const result = await visaCollection.deleteOne(query);
+      res.send(result)
+      console.log(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
